0f203dde2fc86caed909415bab9654915dc48c63
Helmut <helcu@hotmail.com>
2017-03-31 17:25:33 -0500

    primera version del CRUD, sin funciones

---
+++ src/main/java/com/upc/edu/entity/Producto.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.upc.edu.entity;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Producto {
+    
+    private int codigo;
+    private String nombre;
+    
+    
+    
+}
